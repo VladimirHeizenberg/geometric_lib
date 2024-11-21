0416be06a3d5283ede3797d25255cdc26fa4a6c2
Vladimir <volodiakalganov@gmail.com>
2024-11-21 18:09:42 +0300

    tests fixes

--- tests/rectangle.py
+++ tests/rectangle.py
@@ -34,3 +34,35 @@ class RectangleTestCase(unittest.TestCase):
 
         with self.assertRaises(Exception):
             rectangle.area(-10, -20)
+
+    def test_zero_sizes_perimeter(self):
+        perimeter = rectangle.perimeter(0, 0)
+        self.assertEqual(perimeter, 0)
+        perimeter = rectangle.perimeter(0, 10)
+        self.assertEqual(perimeter, 10)
+        perimeter = rectangle.perimeter(10, 0)
+        self.assertEqual(perimeter, 10)
+    
+    def test_positive_sizes_perimeter(self):
+        perimeter = rectangle.perimeter(1, 1)
+        self.assertEqual(perimeter, 4)
+        perimeter = rectangle.perimeter(1, 10)
+        self.assertEqual(perimeter, 22)
+        perimeter = rectangle.perimeter(10, 1)
+        self.assertEqual(perimeter, 22)
+        perimeter = rectangle.perimeter(5, 4)
+        self.assertEqual(perimeter, 18)
+        perimeter = rectangle.perimeter(123, 4243)
+        self.assertEqual(perimeter, 8732)
+        perimeter = rectangle.perimeter(566.2, 12.6)
+        self.assertAlmostEqual(perimeter, 1157,6)
+    
+    def test_negative_sizes_perimeter(self):
+        with self.assertRaises(Exception):
+            rectangle.perimeter(-10, 5)
+
+        with self.assertRaises(Exception):
+            rectangle.perimeter(5, -10)
+
+        with self.assertRaises(Exception):
+            rectangle.perimeter(-10, -20)
